6ba3d29abe80285dbcf416e001910222c002a78f
artplacedeveloper <sketchbook@infoseek.jp>
2018-08-11 16:09:37 +0900

    1st commit

---
+++ XamAppCenterSample2018/Variables.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace XamAppCenterSample2018
+{
+    public static class Variables
+    {
+        // NOTE: Replace this example key with a valid subscription key.
+        public static readonly string ApiKey = "9576c9cc125c4cd18d6e61616ea28d17";
+    }
+}
